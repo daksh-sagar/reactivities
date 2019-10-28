329c32f995da8393030d2986bf26d46232b1f1a5
Daksh <daksh.sagar@yahoo.com>
2019-10-28 13:45:45 +0530

    Add CRUD functionality for activities

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,42 +15,35 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Persistence;
 
-namespace API
-{
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
-    {
+namespace API {
+  public class Startup {
+    public Startup(IConfiguration configuration) {
       Configuration = configuration;
     }
 
     public IConfiguration Configuration { get; }
 
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<DataContext>(opt =>
-      {
+    public void ConfigureServices(IServiceCollection services) {
+      
+      services.AddDbContext<DataContext>(opt => {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
-      services.AddCors(options =>
-      {
-        options.AddPolicy("CorsPolicy", policy =>
-        {
-          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-        });
+      
+      services.AddCors(options => {
+        options.AddPolicy("CorsPolicy",
+          policy => { policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"); });
       });
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
+      services.AddMediatR(typeof(List.Handler).Assembly);
+      
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
 
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+      if (env.IsDevelopment()) {
         app.UseDeveloperExceptionPage();
       }
-      else
-      {
+      else {
         //                app.UseHsts();
       }
 
