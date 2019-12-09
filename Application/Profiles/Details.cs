271592d754f1af23e014903b2c1ac66e7d0ba898
Daksh <daksh.sagar@yahoo.com>
2019-12-09 13:58:34 +0530

    Add functionality to delete photos

---
+++ Application/Profiles/Details.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Profiles {
+  public class Details {
+    public class Query : IRequest<ProfileDto> {
+      public string Username { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, ProfileDto> {
+      private readonly DataContext _context;
+
+      public Handler(DataContext context) {
+        _context = context;
+      }
+
+      public async Task<ProfileDto> Handle(Query request, CancellationToken cancellationToken) {
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName ==
+                                                                 request.Username);
+        
+        return new ProfileDto {
+          DisplayName = user.DisplayName,
+          Username = user.UserName,
+          Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+          Bio = user.Bio,
+          Photos = user.Photos
+        };
+      }
+    }
+  }
+}
---
+++ Application/Profiles/ProfileDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Domain;
+
+namespace Application.Profiles {
+  public class ProfileDto {
+    public string DisplayName { get; set; }
+    public string Username { get; set; }
+    public string Image { get; set; }
+    public string Bio { get; set; }
+    public ICollection<Photo> Photos { get; set; }
+  }
+}
