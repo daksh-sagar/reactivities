80fb54f37de9e1404c6c1d56e0ce65bfc88cf9de
Daksh <daksh.sagar@yahoo.com>
2019-12-15 13:57:46 +0530

    Add Following entity and configure relations

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,8 @@ namespace Persistence {
     public DbSet<UserActivity> UserActivities { get; set; }
     public DbSet<Photo> Photos { get; set; }
     public DbSet<Comment> Comments { get; set; }
+    public DbSet<UserFollowing> Followings { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
       base.OnModelCreating(modelBuilder);
@@ -33,6 +35,19 @@ namespace Persistence {
         .HasOne(a => a.Activity)
         .WithMany(u => u.UserActivities)
         .HasForeignKey(a => a.ActivityId);
+      
+      modelBuilder.Entity<UserFollowing>(b => {
+        b.HasKey(k => new {k.ObserverId, k.TargetId});
+        b.HasOne(o => o.Observer)
+          .WithMany(f => f.Followings)
+          .HasForeignKey(o => o.ObserverId)
+          .OnDelete(DeleteBehavior.Restrict);
+        
+        b.HasOne(o => o.Target)
+          .WithMany(f => f.Followers)
+          .HasForeignKey(o => o.TargetId)
+          .OnDelete(DeleteBehavior.Restrict);
+      });
     }
   }
 }
