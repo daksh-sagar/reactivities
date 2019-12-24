0a3f83cf318c07040ace1c0d5c9acb1872790270
Daksh <daksh.sagar@yahoo.com>
2019-12-24 21:49:35 +0530

    Replace more button with react-infinite-scroller

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { observer } from 'mobx-react-lite'
-import { Grid, Button } from 'semantic-ui-react'
+import { Grid, Loader } from 'semantic-ui-react'
+import InfiniteScroller from 'react-infinite-scroller'
 import ActivityList from './ActivityList'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
 import { RootStoreContext } from '../../../app/stores/rootStore'
@@ -33,18 +34,20 @@ const ActivityDashboard: React.FC = () => {
     <>
       <Grid>
         <Grid.Column width={10}>
-          <ActivityList />
-          <Button
-            floated='right'
-            content='More...'
-            positive
-            onClick={handleGetNext}
-            loading={loadingNext}
-            disabled={totalPages === page + 1}
-          />
+          <InfiniteScroller
+            pageStart={0}
+            loadMore={handleGetNext}
+            initialLoad={false}
+            hasMore={!loadingNext && page + 1 < totalPages}
+          >
+            <ActivityList />
+          </InfiniteScroller>
         </Grid.Column>
         <Grid.Column width={6}>
           <h2>Activity Filters</h2>
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Loader active={loadingNext} />
         </Grid.Column>
       </Grid>
     </>
